a32ea8726ced742bae242800c0a0aea107f442bb
Han Chuan <hanchuan.lim@gmail.com>
2023-09-30 11:30:13 +0800

    Initial files

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+// import { Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./screens/homescreen";
+import SettingsScreen from "./screens/settingsscreen";
+import ContactScreen from "./screens/contactScreen";
+import { FontAwesome } from "@expo/vector-icons";
 
-export default function App() {
+/* function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+ */
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            //Set the icon based on which route it is (name of the tab)
+            if (route.name === "Home") {
+              iconName = focused ? "home" : "home-o";
+            } else if (route.name === "Settings") {
+              iconName = focused? "list" : "list-o";
+            } else if (route.name === "Contact") {
+              iconName = focused? "user" : "user-o";
+            }
+
+            // You can return any component that you like here!
+            return <FontAwesome name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Contacts" component={ContactScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
